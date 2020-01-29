1e62a30ac6cf29ed37a736033e34e82e3205f6d8
logan garcia <garcia.logan@gmail.com>
2020-01-29 08:59:22 -0700

    add console clear

--- SimpleCalculator/Module1.vb
+++ SimpleCalculator/Module1.vb
@@ -9,41 +9,45 @@ Module Module1
     Dim number1 As String
     Dim number2 As String
     Dim operation As String
+    Dim quitProgram As String
+
     Sub Main()
+        Do While True
 
+            Console.WriteLine("Welcome, pick a first number ")
+            number1 = Console.ReadLine()
 
-        Console.WriteLine("Welcome, pick a first number ")
-        number1 = Console.ReadLine()
+            Console.WriteLine("please pick a second number")
+            number2 = Console.ReadLine()
 
-        Console.WriteLine("please pick a second number")
-        number2 = Console.ReadLine()
+            Console.WriteLine("Would you like to add or multiply?")
+            operation = Console.ReadLine()
 
-        Console.WriteLine("Would you like to add or multiply?")
-        operation = Console.ReadLine()
-
-        If operation = "multiply" Then
-            Console.WriteLine(CInt(number1) * CInt(number2))
-            Console.ReadLine()
-            Try
+            If operation = "multiply" Then
                 Console.WriteLine(CInt(number1) * CInt(number2))
-            Catch ex As Exception
-                Console.WriteLine("Please enter a whole number")
+                Console.ReadLine()
+                Try
+                    Console.WriteLine(CInt(number1) * CInt(number2))
+                Catch ex As Exception
+                    Console.WriteLine("Please enter a whole number")
 
-            End Try
+                End Try
 
 
-        ElseIf operation = "add" Then
-            Console.WriteLine(CInt(number1) + CInt(number2))
-            Console.ReadLine()
-            Try
+            ElseIf operation = "add" Then
                 Console.WriteLine(CInt(number1) + CInt(number2))
-            Catch ex As Exception
-                Console.WriteLine("Please enter a whole number")
+                Console.ReadLine()
+                Try
+                    Console.WriteLine(CInt(number1) + CInt(number2))
+                Catch ex As Exception
+                    Console.WriteLine("Please enter a whole number")
 
-            End Try
+                End Try
 
-        End If
+            End If
 
+            Console.Clear()
+        Loop
 
 
 

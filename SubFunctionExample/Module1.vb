eca628b6d657ab6da336716415f6ff2d0b24028b
logan garcia <garcia.logan@gmail.com>
2020-01-30 08:49:52 -0700

    add sub and function

---
+++ SubFunctionExample/Module1.vb
@@ -0,0 +1,31 @@
+﻿
+Option Strict On
+Option Explicit On
+
+
+Module Module1
+
+    Sub Main()
+        Dim mynumber As Integer
+        mynumber = 7
+        Console.WriteLine("In sub MainSub")
+        Console.WriteLine(mynumber)
+        MySub()
+    End Sub
+
+    Sub MySub()
+        Dim mynumber As Integer
+        mynumber = 8
+        Console.WriteLine("In sub MySub")
+        Console.WriteLine(mynumber)
+        Console.ReadLine()
+        Main()
+    End Sub
+
+    Function MyFunc() As Integer
+        Console.WriteLine("In Function MyFunc")
+        Return 0
+        Console.ReadLine()
+    End Function
+
+End Module

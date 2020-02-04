04a44b646006db822afb32cc72e4f23dabdbda3f
logan garcia <garcia.logan@gmail.com>
2020-02-04 08:57:34 -0700

    update pattern

--- SubFunctionExample/Module1.vb
+++ SubFunctionExample/Module1.vb
@@ -7,21 +7,24 @@ Module Module1
 
     Sub Main()
 
-        Dim myNumber As Integer
-        myNumber = 7
-        Console.WriteLine("In Sub Main")
-        Console.WriteLine(myNumber)
-        Console.ReadLine()
+        'Dim myNumber As Integer
+        'myNumber = 7
+        'Console.WriteLine("In Sub Main")
+        'Console.WriteLine(myNumber)
+        'Console.ReadLine()
 
-        MySub(myNumber)
-        Console.WriteLine("In Sub Main")
-        Console.WriteLine(myNumber)
+        'MySub(myNumber)
+        'Console.WriteLine("In Sub Main")
+        'Console.WriteLine(myNumber)
 
-        myNumber = MyFunc(myNumber)
-        Console.WriteLine("In Sub Main")
-        Console.WriteLine(myNumber)
+        'myNumber = MyFunc(myNumber)
+        'Console.WriteLine("In Sub Main")
+        'Console.WriteLine(myNumber)
 
 
+        'Console.ReadLine()
+        'doMath(7, 5, "add")
+        ShowPattern(5)
         Console.ReadLine()
     End Sub
 
@@ -42,4 +45,39 @@ Module Module1
         Console.ReadLine()
         Return myNumber
     End Function
+
+    Sub doMath(firstNumber As Integer, secondNumber As Integer, operatorChoice As String)
+        Dim result As Double
+
+        If operatorChoice = "add" Then
+            result = firstNumber + secondNumber
+            Console.WriteLine(result)
+        ElseIf operatorChoice = "subtract" Then
+            result = firstNumber - secondNumber
+            Console.WriteLine(result)
+        ElseIf operatorChoice = "multiply" Then
+            result = firstNumber * secondNumber
+            Console.WriteLine(result)
+        ElseIf operatorChoice = "divide" Then
+            result = firstNumber / secondNumber
+            Console.WriteLine(result)
+        End If
+
+
+    End Sub
+
+
+    Sub ShowPattern(length As Integer)
+        Dim character As String
+        Dim pattern As String
+
+        character = "*"
+        For index = 0 To length
+            pattern &= character
+            Console.WriteLine(pattern)
+        Next
+        Console.WriteLine()
+
+    End Sub
+
 End Module

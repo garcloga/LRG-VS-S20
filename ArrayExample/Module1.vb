ebd16f80ed24d3c440a8aeea91ead28369125fdd
logan garcia <garcia.logan@gmail.com>
2020-03-03 08:58:47 -0700

    Add Array examples

---
+++ ArrayExample/Module1.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim myArray(3) As Integer
+
+        myArray(1) = 5
+        myArray = {1, 2, 3, 4}
+
+        Console.ReadLine()
+        Dim infoArray() As String
+        infoArray = Split("hello,goodbye,pizza,hungry,ice cream", ",")
+
+        Console.ReadLine()
+    End Sub
+
+
+
+    Private Sub InitiateArrays()
+        Dim numbers(4) As Integer
+        numbers = {1, 2, 4, 8, 16}
+    End Sub
+
+End Module

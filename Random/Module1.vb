77a5879f33ac031dca4fb592005f1ccf9f6f6e65
logan garcia <garcia.logan@gmail.com>
2020-03-09 08:08:44 -0600

    Add example code

---
+++ Random/Module1.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+
+    Sub Main()
+        For j = 1 To 2
+            Console.WriteLine(Timer)
+            For i = 1 To 10
+                Randomize()
+                Console.WriteLine(Str(Rnd()) & "            " & Str(Timer))
+            Next
+        Next
+        Console.ReadLine()
+
+    End Sub
+
+End Module

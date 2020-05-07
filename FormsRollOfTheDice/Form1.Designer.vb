63ad6abc56c26c4205ad8dad05f7742f8d753f1e
logan garcia <garcia.logan@gmail.com>
2020-05-07 14:03:00 -0600

    Add code for form display

--- FormsRollOfTheDice/Form1.Designer.vb
+++ FormsRollOfTheDice/Form1.Designer.vb
@@ -22,41 +22,65 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.ListBox1 = New System.Windows.Forms.ListBox()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RollDiceListBox = New System.Windows.Forms.ListBox()
+        Me.RollButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'ListBox1
+        'RollDiceListBox
         '
-        Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.HorizontalScrollbar = True
-        Me.ListBox1.Location = New System.Drawing.Point(111, 87)
-        Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(343, 186)
-        Me.ListBox1.TabIndex = 0
+        Me.RollDiceListBox.FormattingEnabled = True
+        Me.RollDiceListBox.HorizontalScrollbar = True
+        Me.RollDiceListBox.Location = New System.Drawing.Point(111, 87)
+        Me.RollDiceListBox.Name = "RollDiceListBox"
+        Me.RollDiceListBox.Size = New System.Drawing.Size(343, 186)
+        Me.RollDiceListBox.TabIndex = 0
         '
-        'Button1
+        'RollButton
         '
-        Me.Button1.Location = New System.Drawing.Point(328, 318)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.RollButton.Location = New System.Drawing.Point(111, 309)
+        Me.RollButton.Name = "RollButton"
+        Me.RollButton.Size = New System.Drawing.Size(94, 43)
+        Me.RollButton.TabIndex = 1
+        Me.RollButton.Text = "Roll"
+        Me.RollButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(232, 309)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(94, 43)
+        Me.ClearButton.TabIndex = 2
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(346, 309)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(94, 43)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.RollButton)
+        Me.Controls.Add(Me.RollDiceListBox)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents ListBox1 As ListBox
-    Friend WithEvents Button1 As Button
+    Friend WithEvents RollDiceListBox As ListBox
+    Friend WithEvents RollButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ExitButton As Button
 End Class
--- FormsRollOfTheDice/Form1.vb
+++ FormsRollOfTheDice/Form1.vb
@@ -1,89 +1,85 @@
-﻿Public Class Form1
+﻿'Logan Garcia
+'RCET0265
+'Roll Of The Dice
+'https://github.com/garcloga/LRG-VS-S20
 
-    Public Dice1 As Integer
-    Public Dice2 As Integer
-    Public Sum As Integer
+Option Explicit On
+Option Strict On
 
-    Public One As Integer
-    Public Two As Integer
-    Public Three As Integer
-    Public Four As Integer
-    Public Five As Integer
-    Public Six As Integer
-    Public Seven As Integer
-    Public Eight As Integer
-    Public Nine As Integer
-    Public Ten As Integer
-    Public Eleven As Integer
-    Public Twelve As Integer
-    Public Total As Integer
-    Public x As String
-    Sub Main()
-
+Public Class Form1
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        'Closes the form
+        Me.Close()
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        'Creating an array
-        Dim StoredValues = New Integer(1, 13) {{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}}
+    Private Sub RollButton_Click(sender As Object, e As EventArgs) Handles RollButton.Click
 
-        'Rolling the dice
-        For i = 1 To 1000
+        Dim firstDice As Single
+        Dim secondDice As Single
+        Dim sumOfDice As Single
+        Dim diceArray(10) As Integer
+        Dim columnHeader As String
+        Dim rollColumn As String
+        columnHeader = ""
+        rollColumn = ""
+
+        For roll = 1 To 1000
             Randomize()
-            Dim Dice1 As Integer = CInt(Int((6 * Rnd()) + 1))
+            firstDice = Int((6 * Rnd()) + 1)
             Randomize()
-            Dim Dice2 As Integer = CInt(Int((6 * Rnd()) + 1))
-            Sum = Dice1 + Dice2
+            secondDice = Int((6 * Rnd()) + 1)
+            sumOfDice = firstDice + secondDice
 
-            'Assigning the number totals
-
-            If Sum = 2 Then
-                Two = Two + 1
-            ElseIf Sum = 3 Then
-                Three = Three + 1
-            ElseIf Sum = 4 Then
-                Four = Four + 1
-            ElseIf Sum = 5 Then
-                Five = Five + 1
-            ElseIf Sum = 6 Then
-                Six = Six + 1
-            ElseIf Sum = 7 Then
-                Seven = Seven + 1
-            ElseIf Sum = 8 Then
-                Eight = Eight + 1
-            ElseIf Sum = 9 Then
-                Nine = Nine + 1
-            ElseIf Sum = 10 Then
-                Ten = Ten + 1
-            ElseIf Sum = 11 Then
-                Eleven = Eleven + 1
-            ElseIf Sum = 12 Then
-                Twelve = Twelve + 1
+            'Adds total rolls for each number
+            If sumOfDice = 2 Then
+                diceArray(0) += 1
+            ElseIf sumOfDice = 3 Then
+                diceArray(1) += 1
+            ElseIf sumOfDice = 4 Then
+                diceArray(2) += 1
+            ElseIf sumOfDice = 5 Then
+                diceArray(3) += 1
+            ElseIf sumOfDice = 6 Then
+                diceArray(4) += 1
+            ElseIf sumOfDice = 7 Then
+                diceArray(5) += 1
+            ElseIf sumOfDice = 8 Then
+                diceArray(6) += 1
+            ElseIf sumOfDice = 9 Then
+                diceArray(7) += 1
+            ElseIf sumOfDice = 10 Then
+                diceArray(8) += 1
+            ElseIf sumOfDice = 11 Then
+                diceArray(9) += 1
+            Else
+                diceArray(10) += 1
             End If
-
-            Total = Two + Three + Four + Five + Six + Seven + Eight + Nine + Ten + Eleven + Twelve
-        Next
-        'Indexing the Array
-        Static Dim v As Integer = StoredValues(1, 13)
-
-
-        StoredValues(1, 2) = Two
-        StoredValues(1, 3) = Three
-        StoredValues(1, 3) = Four
-        StoredValues(1, 5) = Five
-        StoredValues(1, 6) = Six
-        StoredValues(1, 7) = Seven
-        StoredValues(1, 8) = Eight
-        StoredValues(1, 9) = Nine
-        StoredValues(1, 10) = Ten
-        StoredValues(1, 11) = Eleven
-        StoredValues(1, 12) = Twelve
-
-
-        For F = 2 To 13
-            ListBox1.Items.Add(StoredValues(1, F))
         Next
 
+        For i = 2 To 12
+            columnHeader &= CStr(i).PadLeft(4) & "|"
+        Next
 
+        'Display the header
+        RollDiceListBox.Items.Add(Space(19) & "Roll of The Dice" & Space(19))
+        RollDiceListBox.Items.Add(StrDup(55, "-"))
+        RollDiceListBox.Items.Add(columnHeader)
+        RollDiceListBox.Items.Add(StrDup(55, "-"))
+
+        'Write the total rolls for each number
+        For i = LBound(diceArray) To UBound(diceArray)
+            rollColumn &= CStr(diceArray(i)).PadLeft(4) & "|"
+        Next
+
+        RollDiceListBox.Items.Add(rollColumn)
+        RollDiceListBox.Items.Add(vbNewLine)
+        RollDiceListBox.TopIndex = RollDiceListBox.Items.Count - 1
 
     End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        RollDiceListBox.Items.Clear()
+
+    End Sub
+
 End Class

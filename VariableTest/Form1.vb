49f2b3946f641ffdef2a69cf811ce26697576a12
logan garcia <garcia.logan@gmail.com>
2020-08-11 01:43:01 -0600

    add form, test experiment (WIP)

---
+++ VariableTest/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+
+    Public word As String
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        word = TextBox1.Text
+    End Sub
+
+    Private Sub Button1_MouseDown(sender As Object, e As MouseEventArgs) Handles Button1.MouseDown
+        Label1.Text = word
+    End Sub
+
+End Class

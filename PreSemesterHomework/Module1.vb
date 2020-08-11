98be44ee3fcee8d348977e7fdc1d1bb137401661
logan garcia <garcia.logan@gmail.com>
2020-08-11 01:37:37 -0600

    add loop to test calculations

---
+++ PreSemesterHomework/Module1.vb
@@ -0,0 +1,13 @@
+﻿Module Module1
+    Public factor As Int32
+    Sub Main()
+        factor = 1
+        For i = 1 To 10
+            factor = factor * i
+            Console.WriteLine(factor)
+        Next
+
+        Console.ReadLine()
+    End Sub
+
+End Module
